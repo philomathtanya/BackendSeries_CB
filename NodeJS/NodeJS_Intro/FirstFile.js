280387895271a951bd09d425044cb222dc46868b
Tanya Bansal <72347751+philomathtanya@users.noreply.github.com>
2023-04-17 23:07:50 +0530

    FirstFile

---
+++ NodeJS/NodeJS_Intro/FirstFile.js
@@ -0,0 +1,50 @@
+console.log("Text from node");
+// Variable declearation
+const a = "Ritesh verma";
+let name = "Ritesh 2";
+console.log(a);
+console.log(name);
+//Arrays Allow
+const arr = [1, 2, 5, 3, 5];
+console.log(arr);
+//Object Allowd
+const data = {
+    name: "ritesh verma",
+    rollnumber: 251,
+    favword: ["coding", "reading books", "GYM"],
+    object: {
+        hello: "guys",
+        bye:"guys"
+    }
+}
+console.log(data);
+//function allowed
+function add(a, b) {
+    return a + b;
+}
+console.log("output: "+add(5,6));
+const square = num => num * num * num;//one line arrow function
+console.log(square(5));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
